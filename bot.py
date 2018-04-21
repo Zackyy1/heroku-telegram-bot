3717923e421b94ffeb4c96c9f9faa3721e98ec83
Zackyy1 <38585862+Zackyy1@users.noreply.github.com>
2018-04-21 16:28:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from flask import jsonify
 from flask import Flask
 
 token = os.environ['TELEGRAM_TOKEN']
-
+server = Flask(__name__)
 firebase = firebase.FirebaseApplication('https://databaserests.firebaseio.com', None)
 
 ######################################## VARIABLES ################################################################################################
@@ -267,4 +267,19 @@ def handleSoup(message):
 
 ########################################################################################################################
 
-bot.polling(none_stop=True)
+
+@server.route('/' + token, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://seljankatest.herokuapp.com/' + token)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
