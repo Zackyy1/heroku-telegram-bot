93af1a04e3fe16dd6b3023e6ad0927d0cb6c2424
Zackyy1 <38585862+Zackyy1@users.noreply.github.com>
2018-04-21 16:47:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -270,11 +270,19 @@ def handleSoup(message):
 ########################################################################################################################
 
 
-PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(token)
-# add handlers
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=token)
-updater.bot.set_webhook("https://seljankatest.herokuapp.com/" + token)
-updater.idle()
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://seljankatest.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
