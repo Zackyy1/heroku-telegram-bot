2a6dbef2bda5b8e30e8af8ff16a0eede40c87bea
Zackyy1 <38585862+Zackyy1@users.noreply.github.com>
2018-04-21 15:42:45 +0300

    Add files via upload

---
+++ parnu.py
@@ -0,0 +1,87 @@
+restaurants = {
+    "wesset" : {
+            'name': "Villa Wesset",
+            'openedEn': "Sun-Thu 8:00-22:00; Fri-Sat 8:00-24:00",
+            'openedEt': "P-N 8:00-22:00; R-L 8:00-24:00",
+            'address': "Supeluse 26",
+            'latitude': "58.378183",
+            'longitude' : '24.499404',
+            'image': "https://static2.visitestonia.com/images/2811668/DSC_5889_.JPG",
+            'priority': 1,
+            'phone':'+372 697 2500',
+            'link':'http://wesset.ee/',
+    },
+    "steffani" : {
+            'name': "Steffani pitsarestoran",
+            'openedEn': "Sun-Thu 8:00-22:00; Fri-Sat 8:00-24:00",
+            'openedEt': "P-N 8:00-22:00; R-L 8:00-24:00",
+            'address': "Nikolai 8",
+            'latitude': "58.375446",
+            'longitude' : '24.497274',
+            'image': "https://static1.visitestonia.com/images/2580463/Steffani_.JPG",
+            'priority': 4,
+            'phone':'+372 443 1170',
+            'link':'http://steffani.ee/et/esileht/'
+    },
+    "BumBum" : {
+            'name': "Bum-Bum pubi",
+            'openedEn': "Sun-Thu 8:00-22:00; Fri-Sat 8:00-24:00",
+            'openedEt': "P-N 8:00-22:00; R-L 8:00-24:00",
+            'address': "Kuninga 1",
+            'latitude': "58.383622",
+            'longitude' : '24.495827',
+            'image': "https://media-cdn.tripadvisor.com/media/photo-s/11/1d/2f/35/caption.jpg",
+            'priority': 3,
+            'phone':'+372 608 0588',
+            'link':'https://www.facebook.com/bumbumpubi/',
+    },
+    "SKADOODLE" : {
+            'name': "Skadoodle",
+            'openedEn': "Sun-Thu 8:00-22:00; Fri-Sat 8:00-24:00",
+            'openedEt': "P-N 8:00-22:00; R-L 8:00-24:00",
+            'address': "Nothing much",
+            'latitude': "58.383622",
+            'longitude' : '24.495827',
+            'image': "https://static.hltv.org/images/galleries/9601-medium/1480781936.635.jpeg",
+            'priority': 5,
+            'phone':'+372215125',
+            'link':'https://www.facebook.com/bumbumpubi/',
+    },
+    "Skyaa" : {
+            'name': "Oleg Vishenka",
+            'openedEn': "en",
+            'openedEt': "et",
+            'address': "Heaven",
+            'latitude': "58.383622",
+            'longitude' : '24.495827',
+            'image': "https://novostivmire.com/wp-content/uploads/2017/05/ffd584b1d56c49d39c931bd369436bee.jpg",
+            'priority': 2,
+            'phone':'+372 215125',
+            'link':'https://www.imstupid.com/',
+    },
+    "peetri pizza" : {
+            'name': "Peetri pizzarestoran",
+            'openedEn': "Sun-Thu 8:00-22:00; Fri-Sat 8:00-24:00",
+            'openedEt': "P-N 8:00-22:00; R-L 8:00-24:00",
+            'address': "Kuninga 10a",
+            'latitude': "58.383622",
+            'longitude' : '24.495827',
+            'image': "https://www.eleven.ee/wp-content/uploads/2014/11/item_35385.jpg",
+            'priority': 3,
+            'phone' : '+44215125',
+            'link' : 'https://www.imstupid.com/',
+    },
+}
+clubs = {}
+
+
+
+
+
+#{
+ #   'Wesset' : {
+  #      'opened1' : 'P-N 241:412',
+   #     'opened2' : 'R-L 123:213'
+    #},
+#
+#}
